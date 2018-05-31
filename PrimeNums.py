1ee6473eff4982d61318560aba9cedafe5e17e32
Freya Rajeshwar <frajeshwar@gmail.com>
2018-05-30 17:11:50 -0700

    Optimizations
    Added two optimizations - doesn't check for even numbers, and doesn't check for factors greater than 1/2 of the number being factored. Makes the code run ~4x faster.

--- PrimeNums.py
+++ PrimeNums.py
@@ -3,21 +3,23 @@
 # by them to see if there is a remainder.
 # Check all numbers less than the number.
 # Calculate the first 50 prime numbers and add them to a list.
+# This uses two optimizations - it doesn't check even numbers, and doesn't check for factors > 1/2 the size of the number being factored
+# This makes the code about 4x faster
 
 # function takes optional boolean to determine whether it will print the output or simply return a list of primes
 def primeNums(print_output = False):
     # we know 2 is the first prime number
     primeList = [2]
     # this is where we start our loop - we start our loop at 2 . i is the number we check to see if prime
-    i = 2
+    i = 1
     # makes sure we only return 50 prime numbers
     while len(primeList) < 50:
-        # until the list is 50 primes in length, keep trying to add primes
-        i +=1
+        # until the list is 50 primes in length, keep trying to add primes, but skipping even numbers (which can't be prime)
+        i +=2
         # initializing (creating / defining) the variable isPrime and making default value to true
         isPrime = True
-        # for every number less than i (which starts at 2 but then increments) and greater than 2, do:
-        for x in range(2,i):
+        # for every number less than i (which starts at 2 but then increments) and greater than 2 but less than half the number, do:
+        for x in range(2,(i+1)/2):
             # initialize var called remainder - check if all numbers less than i are evenly divisible (no remainder)
             remainder = i % x
             if remainder == 0:
